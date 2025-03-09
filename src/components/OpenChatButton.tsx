cc47c1a84c01e9e809fa5f464df74b7a0ffa5a9b
Pietro Schoch <pietro@octoshop.com>
2025-03-09 18:55:46 -0300

    responsive update

--- src/components/OpenChatButton.tsx
+++ src/components/OpenChatButton.tsx
@@ -7,7 +7,7 @@ export function OpenChatButton({ onClick }: OpenChatButtonProps) {
   return (
     <div className="flex items-center gap-2 md:gap-3 fixed bottom-4 right-4 cursor-pointer group hover:bottom-5 transition-all ease-linear" onClick={onClick}>
       {/* Mostra o texto em todas as telas, mas com tamanho adaptado */}
-      <span className="bg-white p-2 md:p-4 rounded-2xl rounded-br-none text-xs md:text-lg leading-tight md:leading-6 w-44 sm:w-64 md:w-80 shadow-lg group-hover:shadow-2xl group-hover:shadow-emerald-500/50 transition-all ease-linear">
+      <span className="bg-white p-2 md:p-4 rounded-2xl rounded-br-none text-xs md:text-lg leading-tight md:leading-6 flex-1 w-[calc(100vw-6rem)] sm:w-64 md:w-80 shadow-lg group-hover:shadow-2xl group-hover:shadow-emerald-500/50 transition-all ease-linear">
         Quer ver uma <strong>Demonstração Gratuita</strong> da Leankeep?
       </span>
       <div className="relative">
